96bac6baf18f75696dc3ae52957aac5f41934fa1
devmatavs <mtsouza.mts@gmail.com>
2022-05-05 21:32:59 -0300

    Conectando com o PostgreSQL + teste HTTP

--- src/main/java/com/dio/santander/bankline/api/model/Movimentacao.java
+++ src/main/java/com/dio/santander/bankline/api/model/Movimentacao.java
@@ -26,6 +26,9 @@ public class Movimentacao {
 	@Enumerated(EnumType.STRING)
 	private MovimentacaoTipo tipo;
 	
+	@Column(name = "id_conta")
+	private Integer idConta;
+	
 	public Integer getId() {
 		return id;
 	}
@@ -57,6 +60,11 @@ public class Movimentacao {
 		this.tipo = tipo;
 	}
 	
+	public Integer getIdConta() {
+		return idConta;
+	}
 	
-	
+	public void setIdConta(Integer idConta) {
+		this.idConta = idConta;
+	}
 }
